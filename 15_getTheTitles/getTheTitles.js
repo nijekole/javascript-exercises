6ee7dbd18fd6ae1c65b2b8337c8f78424c2c5722
Kole <164399230+nijekole@users.noreply.github.com>
2025-11-01 19:51:07 +0100

    Finished

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const nameBooks = books.map(books => books.title);
 
+    return nameBooks;
 };
 
 // Do not edit below this line
