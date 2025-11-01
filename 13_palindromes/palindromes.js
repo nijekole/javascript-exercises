8cc93bb128c93e2a8260c15b970c87a49ced6454
Kole <164399230+nijekole@users.noreply.github.com>
2025-11-01 19:31:28 +0100

    Finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const allchar = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanStr = word.toLowerCase()
+    .split('')
+    .filter((character) => allchar.includes(character))
+    .join(''); 
+
+    const reversedStr = cleanStr.split('').reverse().join('');
+
+    return cleanStr == reversedStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
