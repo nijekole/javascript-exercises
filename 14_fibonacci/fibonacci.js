a529b4f22e193905eac30b27a94c7f2516296cc7
Kole <164399230+nijekole@users.noreply.github.com>
2025-11-01 19:45:57 +0100

    Finished

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+  if (count < 0) {
+    return "OOPS";
+  }
+  const fib = [0, 1];
+  for (let i = 2; i <= count; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[count];
 };
 
 // Do not edit below this line
